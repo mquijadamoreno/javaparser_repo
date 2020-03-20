55217c6e373f856b3ffc9c979d2e9f5752166716
qmmiguel <quijadamorenomiguel@gmail.com>
2020-03-20 11:18:18 +0100

    commit inicial

---
+++ antipatterndetector/src/main/java/um/tfg/antipatterndetector/App.java
@@ -0,0 +1,13 @@
+package um.tfg.antipatterndetector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
